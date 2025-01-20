49240b5bcae03147004687ff1de43aa404099e8e
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2025-01-20 13:31:25 +0300

    register dog and breed model

---
+++ dogs/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+from dogs.models import Breed, Dog
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "size",
+        "friendliness",
+        "trainability",
+        "shedding_amount",
+        "exercise_needs",
+    )
+    list_filter = (
+        "name",
+        "size",
+    )
+    search_fields = ("name",)
+
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "name",
+        "age",
+        "breed",
+        "gender",
+        "color",
+        "favorite_food",
+        "favorite_toy",
+    )
+    list_filter = (
+        "name",
+        "breed",
+    )
+    search_fields = ("name", "breed,")
